d12f977e31a06b30ec52be9cb79fe1e1afbecfcb
revert to avoid formatting <osama.abulkhair@gmail.com>
2022-04-10 10:31:16 +0200

    Remove unused imports. Remove commented code. Use newer annotations in the controller

--- m1-lesson6/src/main/java/com/baeldung/lss/web/controller/UserController.java
+++ m1-lesson6/src/main/java/com/baeldung/lss/web/controller/UserController.java
@@ -5,10 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.validation.BindingResult;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -26,20 +23,19 @@ public class UserController {
         this.userRepository = userRepository;
     }
 
-    //
 
-    @RequestMapping
+    @GetMapping
     public ModelAndView list() {
         Iterable<User> users = this.userRepository.findAll();
         return new ModelAndView("tl/list", "users", users);
     }
 
-    @RequestMapping("{id}")
+    @GetMapping("{id}")
     public ModelAndView view(@PathVariable("id") User user) {
         return new ModelAndView("tl/view", "user", user);
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping
     public ModelAndView create(@Valid User user, BindingResult result, RedirectAttributes redirect) {
         if (result.hasErrors()) {
             return new ModelAndView("tl/form", "formErrors", result.getAllErrors());
@@ -49,20 +45,20 @@ public class UserController {
         return new ModelAndView("redirect:/user/{user.id}", "user.id", user.getId());
     }
 
-    @RequestMapping(value = "delete/{id}")
+    @GetMapping("delete/{id}")
     public ModelAndView delete(@PathVariable("id") Long id) {
         this.userRepository.deleteUser(id);
         return new ModelAndView("redirect:/user/");
     }
 
-    @RequestMapping(value = "modify/{id}", method = RequestMethod.GET)
+    @GetMapping("modify/{id}")
     public ModelAndView modifyForm(@PathVariable("id") User user) {
         return new ModelAndView("tl/form", "user", user);
     }
 
     // the form
 
-    @RequestMapping(params = "form", method = RequestMethod.GET)
+    @GetMapping(params = "form")
     public String createForm(@ModelAttribute User user) {
         return "tl/form";
     }
